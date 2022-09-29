9c789c4b919674806c073af71f7b67e62936031c
Steve Smythe <steve@smythefamily.com>
2022-09-29 16:02:32 -0700

    read food from file

--- eat.py
+++ eat.py
@@ -5,32 +5,30 @@
 # TODO command line interface for number of menus to produce
 
 import random
+import json
 
-
-food = {
-    'almond milk': {'kcal': 30, 'carb': 1, 'fat': 2.5, 'protein': 1, 'sodium': 0, 'max_servings': 999},
-    'honeycrisp apple': {'kcal': 80, 'carb': 22, 'fat': 0, 'protein': 0, 'sodium': 0, 'max_servings': 999},
-    'protein powder 0.5': {'kcal': 60, 'carb': 2, 'fat': 1, 'protein': 12, 'sodium': 53, 'max_servings': 999},
-    'salad with anlvr dressing': {'kcal': 72, 'carb': 10, 'fat': 3, 'protein': 2, 'sodium': 73, 'max_servings': 2},
-    'special k and almond milk': {'kcal': 157, 'carb': 28, 'fat': 4, 'protein': 3, 'sodium': 356, 'max_servings': 999},
-    'toast with hummus': {'kcal': 150, 'carb': 19, 'fat': 6, 'protein': 7, 'sodium': 200, 'max_servings': 999},
-    'tofurky chao mayo sandwich': {'kcal': 389, 'carb': 39, 'fat': 17, 'protein': 28, 'sodium': 777, 'max_servings': 999},
-    'yellow peach': {'kcal': 60, 'carb': 15, 'fat': 0, 'protein': 1, 'sodium': 0, 'max_servings': 999},
-    'yukon gold potato': {'kcal': 110, 'carb': 52, 'fat': 0, 'protein': 6, 'sodium': 0, 'max_servings': 999},
-}
 
 # tuneables
 min_pct = 0.90
 max_kcal = 2000
 min_sodium = 500
 max_sodium = 2000
+food_file = "food.json"
 
 # globals
+food = {}
 keys_params = ['kcal', 'carb', 'fat', 'protein', 'sodium']
 max_params = {}
 min_params = {}
 total = {}
 count = 0
+
+
+def load_food_file():
+    global food
+    # with open('https://site/food.json', 'r') as file:
+    with open(food_file, 'r') as file:
+        food = json.load(file)
 
 
 def init_total_params():
@@ -87,6 +85,7 @@ def params_in_min_max_window():
 
 
 ### MAIN ###
+load_food_file()
 init_params()
 
 while True:
@@ -130,7 +129,7 @@ print(f"Menu: {count} runs")
 print("%-33s kcal %4d, carb %4d, fat %3d, protein %3d, sodium %4d" %
       ("Totals:", total['kcal'], total['carb'], total['fat'], total['protein'], total['sodium']))
 print("%-33s kcal %4d, carb %4d, fat %3d, protein %3d, sodium %4d" %
-      ("Maxs:", max_params['kcal'], max_params['kcal'], max_params['fat'], max_params['protein'], max_params['sodium']))
+      ("Maxs:", max_params['kcal'], max_params['carb'], max_params['fat'], max_params['protein'], max_params['sodium']))
 print(88 * '-')
 for k in sorted(menu.keys()):
     servings = menu[k]
